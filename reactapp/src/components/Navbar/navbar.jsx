dee5046adfe2f6c553e032db7c0143df84cfcb6d
Atthaphon <blance@hotmail.se>
2023-08-25 13:44:33 +0200

    Stod att det inte commitade. Testar igen

--- reactapp/src/components/Navbar/navbar.jsx
+++ reactapp/src/components/Navbar/navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import '/public/css/site.css';
 
+
+
+
 function Navbar() {
+ 
     return (
         <nav className="navbar navbar-expand-lg border-bottom box-shadow">
 
@@ -16,8 +20,10 @@ function Navbar() {
 
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" onClick={() => this.populateArticleData("")}>Hem</a>
+                        <a className="nav-link" onClick={() => populateArticleData("")}>Hem</a>
+
                     </li>
+
 
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
@@ -29,15 +35,16 @@ function Navbar() {
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
 
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("")}>All</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("Halsa")}>Hälsa</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("SamhalleKonflikter")}>Samhälle och konflikt</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("Miljo")}>Miljö</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("VetenskapTeknik")}>Vetenskap & Teknik</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("LivsstillFritt")}>Livsstill & Frittid</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("Ekonomi")}>Ekonomi</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("Religion")}>Religion</a></dt>
-                                <dt className="col"><a className="footer-nav-headline" onClick={() => this.populateArticleData("Idrott")}>Idrott</a></dt>
+
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=Halsa&sortBy=">Hälsa</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=SamhalleKonflikter&sortBy=">Samhälle & Konflikter</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=Miljo&sortBy=">Miljö</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=VetenskapTeknik&sortBy=">Vetenskap & Teknik</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=LivsstillFritt&sortBy=">Livsstil & Fritid</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=Ekonomi&sortBy=">Ekonomi</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=Religion&sortBy=">Religion</a></dt>
+                                <dt className="col"><a className="footer-nav-headline" href="?topic=Idrott&sortBy=">Idrott</a></dt>
+
                                 
                             </div>
 
@@ -50,16 +57,16 @@ function Navbar() {
                         <a className="nav-link" href="#">Nyheter</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Sport</a>
+                        <a className="nav-link" href="?topic=Idrott&sortBy=">Sport</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Kultur</a>
+                        <a className="nav-link" href="?topic=Religion&sortBy=">Kultur</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Livsstil</a>
+                        <a className="nav-link" href="?topic=Halsa&sortBy=">Livsstil</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Teknologi</a>
+                        <a className="nav-link" href="?topic=VetenskapTeknik&sortBy=">Teknologi</a>
                     </li>
                 </ul>
                 <ul className="navbar-nav ml-auto">
