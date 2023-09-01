a6dffdaba79f8b17cb04d51491dea18542a43ec2
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-09-01 11:13:17 +0200

    removed css-files we didn't use

--- reactapp/src/components/SignUpNewsletter/signUp.jsx
+++ reactapp/src/components/SignUpNewsletter/signUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
     return (
         <div style={{ display: 'block' }}>
             <h2 className="signup-text">Come on! Sign up for our newsletter!</h2>
-            <button type="registerButton" className="btn-footer bf-2 align-self-center" onClick={openPopup}>
+            <button type="registerButton" className="btn-footer bf-2" onClick={openPopup}>
                 Signup!
             </button>
             <PopUp isOpen={isPopupOpen} onClose={closePopup} onSubmit={handleEmailSubmit} />
